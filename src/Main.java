f2176fd606026fe37158009a41c42bb5704569ca
KarinaTelles <karinac.telles@gmail.com>
2024-03-21 19:58:37 -0300

    comments

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        ArrayTwo arrayTwo = new ArrayTwo();
-//        arrayTwo.percorreArray();
 //        Array array = new Array();
 //        array.loopArrayHighSeas();
 //        array.loopArrayNumber();
 //        array.loopArrayString();
-        StackOfInteger stackOfInteger = new StackOfInteger();
-        System.out.println("********************************");
-            for (int i = 0; i<100;i++){
-                stackOfInteger.pushFromStack(1 + i);
-                //stupid git credencials
-                //teste maroto
-            }
-        stackOfInteger.printStack();
-        System.out.println("********************************");
-        stackOfInteger.popFromStack();
-        stackOfInteger.printStack();
-        System.out.println("********************************");
+//        array.percorreArray();
+//
+//        StackOfInteger stackOfInteger = new StackOfInteger();
+//        System.out.println("********************************");
+//            for (int i = 0; i<100;i++){
+//                stackOfInteger.pushFromStack(1 + i);
+//                //stupid git credencials
+//                //teste maroto
+//            }
+//        stackOfInteger.printStack();
+//        System.out.println("********************************");
+//        stackOfInteger.popFromStack();
+//        stackOfInteger.printStack();
+//        System.out.println("********************************");
+
     }
 }
