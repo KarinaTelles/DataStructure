4eecc6d43e9e24395946963896d50cba113d67fc
Karina Telles <karina@gmail.com>
2024-03-21 12:10:23 -0300

    kind remind of basic array manipulation

--- src/Array.java
+++ src/Array.java
@@ -2,22 +2,39 @@ import java.util.Arrays;
 
 public class Array {
     String twentyOneArray[] = new String[21];
+    String[] names = new String[10];
     String phantomsOfHighSeas[]=
             {"Dead Men Tell No Tales", "The High Seas", "Edge of World", "Pirates", "The Gallows Jig", "Crossfire", "Oblivion"};
-    int[] numeros = new int[5];
-    String[] nomes = new String[10];
-    int numero = numeros[2]; // Acessa o terceiro elemento do vetor "numeros"
-    String nome = nomes[4]; // Acessa o quinto elemento do vetor "nomes"
-    public void fillArray(){
-        Arrays.fill(numeros, 0); // Atribui o valor 0 a todos os elementos
-        Arrays.fill(nomes, " "); // Atribui a string vazia a todos os elementos
+    int[] numbers = new int[5];
+    int three = 3;
+    String karina = "Karina";
+    String nome = names[4]; // Acessa o quinto elemento do vetor "nomes"
+    public void fillArrayWithZero(){
+        Arrays.fill(numbers, 0); // Atribui o valor 0 a todos os elementos
     }
-    public void loopArray(){
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.println(numeros[i]);
+    public void fillArrayWithBlank(){
+        Arrays.fill(names, " "); // Atribui a string vazia a todos os elementos
+    }
+    public void loopArrayHighSeas(){
+        for (int i = 0; i < phantomsOfHighSeas.length; i++) {
+            System.out.println(i+ " - " + phantomsOfHighSeas[i]);
         }
-        for (String nome : nomes) {
-            System.out.println(nome);
+        for (String phs : phantomsOfHighSeas) {
+            System.out.print(phs + ", ");
+        }
+    }
+    public void loopArrayNumber(){
+        fillArrayWithZero();
+        numbers[2] = three;
+        for (int nmb : numbers){
+            System.out.println(nmb);
+        }
+    }
+    public void loopArrayString(){
+        fillArrayWithBlank();
+        names[0] = karina;
+        for(String nm : names){
+            System.out.println(nm);
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        ArrayTwo arrayTwo = new ArrayTwo();
-        arrayTwo.percorreArray();
+//        ArrayTwo arrayTwo = new ArrayTwo();
+//        arrayTwo.percorreArray();
+        Array array = new Array();
+//        array.loopArrayHighSeas();
+//        array.loopArrayNumber();
+        array.loopArrayString();
     }
 }
