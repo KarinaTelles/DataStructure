36f9c88160fc9054800fea7b2dbc7534e787ac98
KarinaTelles <karinac.telles@gmail.com>
2024-03-22 11:44:09 -0300

    Just testing queue implementation

--- src/geraldatastructure/Queue.java
+++
@@ -1,5 +0,0 @@
-package geraldatastructure;
-
-public class Queue {
-
-}
---
+++ src/geraldatastructure/QueueImplementation.java
@@ -0,0 +1,7 @@
+package geraldatastructure;
+
+import java.util.LinkedList;
+
+public class Queue {
+    Queue<String> queue = new LinkedList<>();
+}
